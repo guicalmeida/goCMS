69a42afcac7dedbd8cd909ca5758e8bf77f8c1f9
guicalmeida <guica.almeida@gmail.com>
2023-01-20 18:59:01 -0300

    feat: serve index file as entry template

--- main.go
+++ main.go
@@ -2,11 +2,21 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
 	"github.com/guicalmeida/goCMS/database"
 )
 
 func main() {
 	database.ConnectToDB()
 	fmt.Println("iniciando servidor REST com Go")
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*.tmpl")
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"title": "Main website",
+		})
+	})
+	router.Run(":9254")
 }
